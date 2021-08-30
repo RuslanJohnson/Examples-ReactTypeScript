30e862f91f904da09dafc70069adb472c7c3f0f9
RuslanJohnson <66631440+RuslanJohnson@users.noreply.github.com>
2021-08-30 04:08:29 +0300

    Modify elements by styled-component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
 import { BrowserRouter, NavLink } from "react-router-dom";
 import AppRouter from "./components/AppRouter";
 import "./styles/index.css";
+import styled from "styled-components";
 
 function App() {
+	const Navbar = styled.div`
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 50px;
+		background-color: #3b52d8;
+	`;
+
+	const StyledNavLink = styled(NavLink)`
+		color: #fff;
+		text-decoration: none;
+		margin: 15px;
+	`;
+
 	return (
 		<BrowserRouter>
-			<div className="navbar">
-				<NavLink to="/users">Users</NavLink>
-				<NavLink to="/todos">Todos</NavLink>
-			</div>
+			<Navbar>
+				<StyledNavLink to="/users">Users</StyledNavLink>
+				<StyledNavLink to="/todos">Todos</StyledNavLink>
+			</Navbar>
 			<AppRouter />
 		</BrowserRouter>
 	);
